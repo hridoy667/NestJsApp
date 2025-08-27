e8be2dd79d2248f6c1b69fafd836fecae534b8eb
Ali Imam Hridoy <hridoy15-14667@diu.edu.bd>
2025-08-27 23:52:53 +0600

    pg connected and user entity created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type:'postgres',
+      url:process.env.DATABASE_URL,
+      autoLoadEntities:true,
+      synchronize: true
+    }),
+    UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.entity.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class User{
+    @PrimaryGeneratedColumn()
+    id:number;
+
+    @Column()
+    name:string
+    
+}
